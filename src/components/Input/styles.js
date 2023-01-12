1aee0efe16ef7a69b0cf249c6d058c09cb2271ed
Renan Franca <renanfranca.dev@gmail.com>
2023-01-12 14:28:20 -0300

    Parte de Login e banco de dados concluída

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -23,3 +23,9 @@ export const InputText = styled.input`
     height: 30px;
     
 `
+
+export const ErrorText = styled.p`
+    color: #FF0000;
+    fonte-size:12px;
+    margin: 5px 0 5px 0;
+`
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,22 +1,41 @@
-// import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md';
+import { useNavigate  } from "react-router-dom";
+import { MdEmail, MdLock } from 'react-icons/md'
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
-import { useNavigate } from 'react-router-dom';
-// import { api } from '../../services/api';
-
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { api } from "../../services/api";
 
 import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+
+const schema = yup.object({
+    email: yup.string().email('email não valido').required('Campo obrigatório'),
+    password: yup.string().min(5, 'mínimo de 3 caracteres').required('Campo obrigatório'),
+  }).required();
+
 
 const Login = () => {
 
         const navigate = useNavigate();
-    
-        const handleClickSignIn = () => {
-            navigate('/feed')
 
-        }
+        const { control, handleSubmit, formState: { errors } } = useForm({
+            resolver: yupResolver(schema),
+            mode: 'onChange',
+        });
+
+            const onSubmit = async formData => {
+                try{
+                    const {data} = await api.get(`users?email=${formData.email}&senha=${formData.password}`);
+                    if(data.length === 1){
+                        navigate('/feed')
+                    }
+                }catch{
+                    alert('ERROR, favor comunicar ao suporte')
+                }
+            };
+
     
 
     return (<>
@@ -30,12 +49,12 @@ const Login = () => {
                 <Wrapper>
                 <TitleLogin>Faça seu cadastro</TitleLogin>
                 <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                <form >
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"   />
-                   
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha"  />
-                    
-                    <Button title="Entrar" variant="secondary" type="submit" onClick={handleClickSignIn}/>
+                 <form onSubmit={handleSubmit(onSubmit)}>
+                    <Input name="email" control={control} placeholder="E-mail" leftIcon={<MdEmail />}
+                    errorMessage={errors?.email?.message}/>
+                    <Input name="password" control={control} type="password" placeholder="Senha" leftIcon={<MdLock />}
+                     errorMessage={errors?.password?.message}/>
+                    <Button title="Entrar" variant="secondary" type="submit"/>
                 </form>
                 <Row>
                     <EsqueciText>Esqueci minha senha</EsqueciText>
@@ -43,8 +62,9 @@ const Login = () => {
                 </Row>
                 </Wrapper>
             </Column>
-        </Container>''
+        </Container>
     </>)
 }
 
 export { Login }
+
